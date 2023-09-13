5f41e2ba697f812516b356593a5d3f7f0f9a684d
Matheus Grecco <matheusgrecco@hotmail.com>
2023-09-12 21:52:07 -0300

    Finished the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,49 +1,57 @@
 import React, { useEffect, useState } from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import axios from 'axios'
 
 const HomeScreen = ({ navigation }) => {
   const [jokes, setJokes] = useState([])
-  useEffect(() => {
-    async function fetchJokes() {
-      try {
-        const response1 = await axios.get(
-          'https://api.chucknorris.io/jokes/random'
-        )
-        const response2 = await axios.get(
-          'https://api.chucknorris.io/jokes/random'
-        )
 
-        const joke1 = response1.data.value
-        const joke2 = response2.data.value
+  const fetchJokes = async () => {
+    try {
+      const response1 = await axios.get(
+        'https://api.chucknorris.io/jokes/random'
+      )
+      const response2 = await axios.get(
+        'https://api.chucknorris.io/jokes/random'
+      )
 
-        setJokes([joke1, joke2])
-      } catch (error) {
-        console.error('Erro ao buscar piadas:', error)
-      }
+      const joke1 = response1.data.value
+      const joke2 = response2.data.value
+
+      // Atualize o estado para incluir o texto de cada piada
+      setJokes([joke1, joke2])
+    } catch (error) {
+      console.error('Erro ao buscar as piadas:', error)
     }
+  }
 
+  useEffect(() => {
     fetchJokes()
   }, [])
 
   const handleLogin = () => {
-    if (handleLogin) {
-      navigation.navigate('Second Page')
-    }
+    navigation.navigate('Second Page')
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Piadas do Chuck Norris</Text>
       <View style={styles.input}>
-        {jokes.map((joke, index) => (
-          <Text key={index} style={styles.jokeText}>
-            {joke}
-          </Text>
-        ))}
+        <Text style={styles.title}>Piadocas do Chuck Norris</Text>
       </View>
-      <Button title="Recarregar Piadas" onPress={() => fetchJokes()} />
+
+      {/* Mapeie as piadas e renderize o texto de cada piada */}
+      {jokes.map((joke, index) => (
+        <View key={index} style={styles.jokeContainer}>
+          <Text style={styles.jokeText}>{joke}</Text>
+        </View>
+      ))}
+
+      <TouchableOpacity
+        style={styles.button}
+        title="Recarregar Piadas"
+        onPress={fetchJokes}
+      >
+        <Text style={styles.buttonText}>Recarregar novas piadocas</Text>
+      </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}> Ir para a segunda tela </Text>
@@ -61,26 +69,36 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    marginBottom: 16
+    marginBottom: 16,
+    color: '#f1a35a'
   },
   input: {
     width: '80%',
-    height: 40,
-    borderColor: '#f1a35a',
-    borderWidth: 1,
-    marginBottom: 20,
-    marginTop: 8,
-    paddingHorizontal: 8,
-    borderRadius: 10
+    height: 50,
+    marginBottom: 20
   },
   button: {
     backgroundColor: '#f1a35a',
     paddingVertical: 10,
     paddingHorizontal: 20,
-    borderRadius: 10
+    borderRadius: 10,
+    marginBottom: 15,
+    width: '70%',
+    alignItems: 'center'
   },
   buttonText: {
     fontSize: 15
+  },
+  jokeContainer: {
+    width: '80%',
+    marginBottom: 20,
+    backgroundColor: '#e0e0e0', // Cor de fundo para as piadas
+    borderRadius: 10,
+    padding: 10
+  },
+  jokeText: {
+    fontSize: 16,
+    color: '#333' // Cor do texto das piadas
   }
 })
 
--- screens/SecondScreen.js
+++ screens/SecondScreen.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>você chegou cara o/</Text>
-      <StatusBar style="light" />
     </View>
   )
 }
@@ -22,22 +20,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     marginBottom: 16
   },
-  input: {
-    width: '80%',
-    height: 40,
-    borderColor: '#f1a35a',
-    borderWidth: 1,
-    marginBottom: 20,
-    marginTop: 8,
-    paddingHorizontal: 8,
-    borderRadius: 10
-  },
-  button: {
-    backgroundColor: '#f1a35a',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10
-  },
+  input: {},
   buttonText: {
     fontSize: 15
   }
